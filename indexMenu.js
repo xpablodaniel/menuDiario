bc30776801bc30c5b58083dcbd235441ecb53512
hotel23demayo <hotel23demayo@suteba.org.ar>
2023-06-16 03:13:42 -0300

    Agregando archivo y cambios en el proyecto

--- indexMenu.js
+++ indexMenu.js
@@ -1,52 +1,46 @@
-
 const almuerzoMenu = [
-    "Empanadas Criollas", "Ensalada Capresse", "Ensalada Lucho’s Cheff", "Tortillin de Papa", "Papas Cheddar y Puerro" , "Ensalada del Cheff" , 
-    "Tortilla Española con Ensaladilla"
-    // "Ensalada de pollo a la parrilla",
-    // "Sándwich de atún con ensalada de col",
-    // "Pizza vegetariana con champiñones y aceitunas"
-];
-almuerzoMenu.push("Crunch de Pollo con Ali - Oli");
-const cenaMenu = [
-    "Muslo Braseado al Limón con Ensalada del Cheff", "Milanesa de Peceto con Puré Marmolado de Espinacas", "Ravioli de Espinaca Mediterráneo",
-     "Suprema de Pollo con Arroz Pilaf", "Carne al Horno Braseada a la Criolla con Mix de Hojas Verdes", "Pechugas al Verdeo", "Tallarines Mediterraneo"
-    // "Pasta con salsa de tomate y albóndigas",
-    // "Pollo a la cordon bleu con puré de papas",
-    // "Salmón a la parrilla con espárragos asados"
-];
-cenaMenu.push("Carne al Horno Braseada con Ensalada");
-const postreMenu = [
-    "Flan de Naranja con Dulce de Leche", "Ensalada de frutas de Estación", "Helado de Americana con Praline de Frutos Secos",
-    " Budín de Pan con Crema Montada", "Bombón Suizo", "Brownie", "Ensalada de Frutas con Crema", 
-    // "Pastel de manzana con crema batida",
-    // "Helado de vainilla con salsa de chocolate",
-    // "Flan casero con caramelo"
-];
-
-
-postreMenu.push("Manzanas Asadas con Crema");
-
-const almuerzoSelect = document.getElementById('almuerzo-select');
-const cenaSelect = document.getElementById('cena-select');
-const postreSelect = document.getElementById('postre-select');
-
-// función para agregar opciones al menú desplegable
-function addToSelect(item, select) {
+    "Empanadas Criollas", "Ensalada Capresse", "Ensalada Lucho’s Cheff", "Tortillin de Papa", "Papas Cheddar y Puerro", "Tortilla Española con Ensaladilla",
+    "Papines Rellenos", "Bruschetta Mediterránea", "Ensalada Caesar", "Crujientes de Muzzarella", "Quesadillas con Pico de Gallo", "Papas Cheddar y Panceta"
+  ];
+  almuerzoMenu.push("Crunch de Pollo con Ali - Oli");
+  
+  const cenaMenu = [
+    "Muslo Braseado al Limón con Ensalada del Cheff", "Milanesa de Peceto a la Suiza", "Ravioli de Espinaca Mediterráneo",
+    "Suprema de Pollo con Arroz Pilaf", "Pechugas al Verdeo", "Tallarines Mediterráneo",
+    "Muslo al Ajillo con Puré de Batatas", "Lasagna de Carne y Verduras", "Suprema Mariland", "Carré de Cerdo a la Riojana"
+  ];
+  cenaMenu.push("Carne al Horno Braseada");
+  
+  const postreMenu = [
+    "Flan de Naranja con Dulce de Leche", "Helado de Americana con Praline de Frutos Secos",
+    " Budín de Pan con Crema Montada", "Bombón con Salsa de Frutos Rojos", "Brownie de Chocolate", "Ensalada de Frutas con Crema", "Mousse de Chocolate", "Crepe de Frutas con Crema", "Flan Casero Mixto",
+    "Crumble de Manzana y Helado"
+  ];
+  postreMenu.push("Manzanas Asadas con Crema");
+  
+  // Ordenar los menús alfabéticamente
+  almuerzoMenu.sort();
+  cenaMenu.sort();
+  postreMenu.sort();
+  
+  const almuerzoSelect = document.getElementById('almuerzo-select');
+  const cenaSelect = document.getElementById('cena-select');
+  const postreSelect = document.getElementById('postre-select');
+  
+  // función para agregar opciones al menú desplegable
+  function addToSelect(item, select) {
     let option = document.createElement('option');
     option.textContent = item;
     select.appendChild(option);
-}
-
-// agregar opciones a los menús desplegables
-
-almuerzoMenu.forEach(item => addToSelect(item, almuerzoSelect));
-cenaMenu.forEach(item => addToSelect(item, cenaSelect));
-postreMenu.forEach(item => addToSelect(item, postreSelect));
-
-
-window.addEventListener('load', function() {
+  }
+  
+  // agregar opciones a los menús desplegables
+  almuerzoMenu.forEach(item => addToSelect(item, almuerzoSelect));
+  cenaMenu.forEach(item => addToSelect(item, cenaSelect));
+  postreMenu.forEach(item => addToSelect(item, postreSelect));
+  
+  window.addEventListener('load', function () {
     almuerzoSelect = document.getElementById('almuerzo-select');
     cenaSelect = document.getElementById('cena-select');
     postreSelect = document.getElementById('postre-select');
-});
-
+  });
